42f9b0cc02c6fb926bff5410e591ec596611afed
moumao <1195358569@qq.com>
2017-05-24 17:24:57 +0800

    update
    搭建好舞台

--- cfg/test.js
+++ cfg/test.js
@@ -35,6 +35,10 @@ module.exports = {
             path.join(__dirname, '/../test')
           ]
         )
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
       }
     ]
   },
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,33 @@ require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+
+//获取图片相关数据
+let imageDates = require('../data/imageDates.json');
+
+//将图片信息转换成URL路径信息
+let genImageUrl = (imageDatasArr)=>{
+  for (let i=0,j=imageDatasArr.length;i<j;i++){
+    let singleImageData = imageDatasArr[i];
+    singleImageData.imageURL= require('../images/'+singleImageData.filename);
+    imageDatasArr[i]=singleImageData;
+  }
+  return imageDatasArr
+};
+
+imageDates = genImageUrl(imageDates);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+     <section className="stage">
+       <section className="img-sec">
+
+       </section>
+       <nav className="controller-nav">
+
+       </nav>
+     </section>
     );
   }
 }
